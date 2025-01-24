37686e5e2c0c98af89fcdfab98dd4d8fdcb8ba9e
mark wonnacott <ragzouken@gmail.com>
2025-01-24 16:30:44 +0100

    Build and publish APK in workflows (#71)

--- conda/bld.bat
+++ conda/bld.bat
@@ -1,9 +1,9 @@
 REM @echo on
 
 mkdir %SCRIPTS%
-robocopy %RECIPE_DIR%\artifacts\StandaloneWindows64 %SCRIPTS%\NanoverImd /e
+robocopy %RECIPE_DIR%\artifacts\StandaloneWindows64 %SCRIPTS%\NanoVer-iMD-VR /e
 REM Make NanoverImd available in the Path while keeping it in
 REM its directory.
 set local_script=%%CONDA_PREFIX%%\Scripts%
-echo "%local_script%\NanoverImd\Nanover iMD.exe" > %SCRIPTS%\NanoverImd.bat
+echo "%local_script%\NanoVer-iMD-VR\NanoVer iMD.exe" > %SCRIPTS%\NanoVer-iMD-VR.bat
 exit 0
